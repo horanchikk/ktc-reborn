df7bb2097d31ded1e6bd363bc24561742e142e1b
horanchikk <horandev.service@gmail.com>
2024-10-21 16:07:51 +0700

    refactor(middleware): Добавлена деструктуризация данных

--- middleware/user-only.ts
+++ middleware/user-only.ts
@@ -2,8 +2,8 @@ import { useUser } from '~/store/useUser'
 
 export default defineNuxtRouteMiddleware(() => {
   if (import.meta.client) {
-    const app = useNuxtApp()
-    const user = useUser(app.$pinia)
+    const { $pinia } = useNuxtApp()
+    const user = useUser($pinia)
 
     if (user.data.access_token === undefined) {
       return navigateTo('/auth')
