59c222b6f68bfd565dc760fcdf46b8ddb1c4be0d
horanchikk <horandev.service@gmail.com>
2024-10-22 11:47:28 +0700

    fix(markup): Обновлена верстка страницы ошибки

--- error.vue
+++ error.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="flex flex-col w-screen h-screen bg-background-100 text-foreground">
-    <main class="flex-auto flex flex-col items-center justify-center">
-      <p class="font-bold text-3xl">
-        Возникла ошибка 🙁
-      </p>
-      <button
-        class="mt-3 border-[1px] text-xl px-10 border-primary hover:bg-primary hover:text-black rounded-xl flex gap-3 justify-center items-center duration-150"
-        @click="router.back()"
-      >
-        Вернуться обратно
-      </button>
+    <main class="flex-auto show flex justify-center items-center">
+      <div class="w-[413px] h-full flex flex-col gap-3 items-center justify-center">
+        <p class="font-bold text-4xl">
+          Возникла ошибка
+        </p>
+        <p class="text-xl text-center">
+          Не переживайте, мы уже знаем<br>о проблеме и решаем её 😊
+        </p>
+        <button
+          class="mt-3 border-[1px] text-md px-10 border-primary hover:bg-primary hover:text-black rounded-xl flex gap-3 justify-center items-center duration-150"
+          @click="router.back()"
+        >
+          Вернуться обратно
+        </button>
+      </div>
     </main>
   </div>
 </template>
