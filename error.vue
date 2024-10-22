888b77e8d5d4e8080e8495d3d8778cbeaf7162c4
horanchikk <horandev.service@gmail.com>
2024-10-22 11:09:34 +0700

    fix(markup): Обновлена верстка страницы ошибки

--- error.vue
+++ error.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-col w-screen h-screen bg-background-100 text-foreground">
-    <main class="flex-auto text-8xl font-bold flex flex-col items-center justify-center">
-      <p>404</p>
+    <main class="flex-auto flex flex-col items-center justify-center">
+      <p class="font-bold text-3xl">
+        Возникла ошибка 🙁
+      </p>
       <button
-        class="mt-3 border-[1px] font-semibold text-xl px-10 border-primary hover:bg-primary hover:text-black rounded-xl flex gap-3 justify-center items-center duration-150"
+        class="mt-3 border-[1px] text-xl px-10 border-primary hover:bg-primary hover:text-black rounded-xl flex gap-3 justify-center items-center duration-150"
         @click="router.back()"
       >
         Вернуться обратно
