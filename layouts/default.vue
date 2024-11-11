85dd6c2d3f470b41eeea42e1233816048a11c2fc
horanchikk <horandev.service@gmail.com>
2024-11-11 11:57:10 +0700

    feature(markup): добавлено изменение цветов для nav/notif баров

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,7 @@
 
 <script setup lang="ts">
 import { useSwipe } from '@vueuse/core'
+import { updateColors } from '~/composables/useColors'
 import { useSideBar } from '~/store/useSideBar'
 
 const store = useSideBar()
@@ -22,5 +23,9 @@ const { direction, lengthX } = useSwipe(globalWindow, {
   onSwipeEnd() {
     if (direction.value === 'right' && lengthX.value < -100) store.show()
   },
+})
+
+onMounted(() => {
+  updateColors('#212121')
 })
 </script>
