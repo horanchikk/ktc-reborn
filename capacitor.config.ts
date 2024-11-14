a56dac97127db48570ca2396c699a39df3c0b093
horanchikk <horanchikk@kansk-tc.ru>
2024-11-14 20:43:00 +0700

    refactor(name): Изменено название проекта

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'app.hapticx.ktc',
-  appName: 'ktc-reborn',
+  appId: 'app.hapticx.procollege',
+  appName: 'ProCollege',
   webDir: '.output/public',
   zoomEnabled: false,
 }
