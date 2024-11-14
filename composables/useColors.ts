0fb7cb5e9809c2021da33ee04a2a771e1a86e781
horanchikk <horanchikk@kansk-tc.ru>
2024-11-15 00:30:14 +0700

    feature(capacitor): добавлена обработка платформы для изменения цвета

--- composables/useColors.ts
+++ composables/useColors.ts
@@ -1,7 +1,10 @@
 import { StatusBar } from '@capacitor/status-bar'
 import { NavigationBar } from '@capgo/capacitor-navigation-bar'
+import { Capacitor } from '@capacitor/core'
 
 export function updateColors(color: string) {
-  StatusBar.setBackgroundColor({ color })
-  NavigationBar.setNavigationBarColor({ color })
+  if (Capacitor.getPlatform() === 'android') {
+    StatusBar.setBackgroundColor({ color })
+    NavigationBar.setNavigationBarColor({ color })
+  }
 }
