9df97948b35538a6c52622cdb42ce9725b5d7187
horanchikk <horandev.service@gmail.com>
2024-11-14 21:14:55 +0700

    feature(markup): обновление рендера блога

--- components/Blog/Post.vue
+++ components/Blog/Post.vue
@@ -2,18 +2,18 @@
   <div class="flex flex-col gap-2 text-foreground bg-background-100 rounded-lg overflow-hidden show">
     <div class="flex gap-4 items-center m-2">
       <img
-        :src="$props.avatar"
-        :alt="`Avatar of ${$props.author}`"
+        :src="props.avatar"
+        :alt="`Avatar of ${props.author}`"
         class="bg-white rounded-full"
       >
-      <p v-text="$props.author" />
+      <p v-text="props.author" />
     </div>
     <div
       class="news-content px-2"
-      v-html="$props.raw_content"
+      v-html="props.raw_content"
     />
     <div class="w-full text-right -mt-2 px-2 pb-2 text-sm font-medium opacity-50">
-      {{ $props.date }}
+      {{ props.date }}
     </div>
   </div>
 </template>
@@ -28,5 +28,5 @@ interface PostList {
   attachments: []
 }
 
-defineProps<PostList>()
+const props = defineProps<PostList>()
 </script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full overflow-y-scroll flex flex-col p-2 gap-5">
+  <div class="w-full overflow-y-scroll flex flex-col p-2 gap-5 mb-[100px]">
     <NuxtLink to="/blog/new">
       <button class="absolute bottom-5 right-5 bg-primary hover:opacity-50 p-4 z-20 rounded-full flex items-center justify-center duration-150">
         <IPencil class="w-10 h-10" />
@@ -18,7 +18,7 @@
     </div>
     <template v-else>
       <BlogPost
-        v-for="(post, idx) in postList"
+        v-for="(post, idx) in postList.items"
         :key="idx"
         :avatar="post.avatar"
         :title="post.title"
@@ -47,7 +47,7 @@ definePageMeta({
 })
 
 const api = useApi()
-const postList = ref<PostList[] | null>(null)
+const postList = ref<{ items: PostList[] } | null>(null)
 
 onMounted(async () => {
   postList.value = await api.get('/blogs/')
