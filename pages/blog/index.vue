ff21349f4e00d85b3d3c17bcb149f077cae458db
horanchikk <horanchikk@kansk-tc.ru>
2024-11-15 10:37:32 +0700

    fix(markup): исправлен skeleton для блога

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="w-full overflow-y-scroll flex flex-col p-2 gap-5 mb-[100px]">
-    <NuxtLink to="/blog/new">
+    <!-- <NuxtLink to="/blog/new">
       <button class="absolute bottom-5 right-5 bg-primary hover:opacity-50 p-4 z-20 rounded-full flex items-center justify-center duration-150">
         <IPencil class="w-10 h-10" />
       </button>
-    </NuxtLink>
+    </NuxtLink> -->
     <div
       v-if="postList === null"
       class="w-full h-full"
@@ -13,7 +13,7 @@
         v-for="i of Array.from({ length: 10 }).map((i, idx) => idx)"
         :key="i"
         :class="i === 0 ? 'mt-0 mb-4' : 'my-4'"
-        class="w-full h-64 loading rounded-md opacity-30"
+        class="w-full h-32 loading rounded-md opacity-30"
       />
     </div>
     <template v-else>
