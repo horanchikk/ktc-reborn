c5c63b15b7d5a36f358451ce5191c675f8b10204
horanchikk <horandev.service@gmail.com>
2025-04-28 19:19:24 +0200

    feat: добавлена логика работа OTA-обновлений

--- repository/modules/ota.ts
+++ repository/modules/ota.ts
@@ -2,11 +2,10 @@ import { API } from '../api'
 import type { Tota } from '~/types/ota'
 
 class OTAModule extends API {
-  async getVersion(isDev: boolean = false) {
-    return await this.get<Tota>(
-      `/updates/check${isDev ? '?prerelease=true' : ''}`,
-    )
+  async getVersion(isDev: boolean) {
+    return await this.get<Tota>(`/updates/check${isDev ? '?prerelease=true' : ''}`)
   }
 }
 
 export default OTAModule
+
