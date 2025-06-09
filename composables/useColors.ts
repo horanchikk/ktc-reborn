4f9690855099ee351cc9717374943a5e1530c7f4
horanchikk <horanchikk@kansk-tc.ru>
2025-06-10 01:57:21 +0700

    fix(colors): добавлено логирование ошибок при установке цвета статус-бара и навигационной панели на Android

--- composables/useColors.ts
+++ composables/useColors.ts
@@ -1,10 +1,15 @@
-import { StatusBar } from '@capacitor/status-bar'
+import { StatusBar, Style } from '@capacitor/status-bar'
 import { NavigationBar } from '@capgo/capacitor-navigation-bar'
 import { Capacitor } from '@capacitor/core'
 
 export function updateColors(color: string) {
+  const log = useLogger('useColors');
+
   if (Capacitor.getPlatform() === 'android') {
-    StatusBar.setBackgroundColor({ color })
-    NavigationBar.setNavigationBarColor({ color })
+    StatusBar.setStyle({
+      style: Style.Dark
+    })
+    StatusBar.setBackgroundColor({ color }).catch(e => log.error(e))
+    NavigationBar.setNavigationBarColor({ color }).catch(e => log.error(e))
   }
 }
